b1abb51e9b2d10ad19f6ea9d9d721826a53e3a80
sh1mc <sh1@coins.tsukuba.ac.jp>
2021-07-15 05:36:38 +0900

    nanka ugokan

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `sh1mc のウェブサイト`,
+    description: `sh1mc's website`,
+    author: `sh1mc`,
+    siteUrl: `https://www.sh1m.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,10 +11,39 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `md`,
+        path: `${__dirname}/src/md`,
+      },
+    },
+	{
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
+
+	{
+		resolve: `gatsby-transformer-remark`,
+		options: {
+			commonmark: true,
+			footnotes: true,
+			pedantic: true,
+			gfm: true,
+			plugins: [
+				{
+					resolve: `gatsby-remark-relative-images`
+				},
+				{
+					resolve: `gatsby-remark-images`,
+					options: {
+						maxwidth: 600
+					}
+				},
+
+			]
+		}
+	},
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -26,7 +55,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/sh1mc_icon.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,47 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+export const query = graphql`
+	query {
+		allMarkdownRemark {
+			nodes {
+				frontmatter {
+					title
+				}
+				html
+			}
+		}
+	}
+`
+
+const IndexPagea = ({ data }) => {
+	<Layout>
+		{data.allMarkdownRemark.nodes.map(node => (
+			<div key={node.id}>
+				<h1>{node.frontmatter.title}</h1>
+				<div dangerouslySetInnerHTML={{ __html: node.html }} />
+			</div>
+		))}
+	</Layout>
+}
+
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+	<Seo title="Portfolio" />
+	<h1>ポートフォリオ</h1>
+	<p></p>
+	<StaticImage
+	  src="../images/sh1mc_icon.jpg"
+	  width={150}
+	  quality={95}
+	  formats={["AUTO", "WEBP", "AVIF"]}
+	  alt="sh1mc's icon"
+	/>
   </Layout>
 )
 
-export default IndexPage
+export default IndexPagea
